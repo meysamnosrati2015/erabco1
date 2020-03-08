7741ad61213bc89e7d84e629f3ab427bc439bfca
meysam <maisamnosraty@gmail.cm>
2020-03-08 21:47:32 +0330

    init start project php mvc in github

--- application/system/Application.php
+++ application/system/Application.php
@@ -6,9 +6,29 @@ namespace App\system;
 
 class Application
 {
+    /**
+     * @var string
+     */
+    private $uri = "/";
+    private $url;
+
     public function __construct()
     {
-        echo 'application';
+     $this->processUrl();
+     var_dump($this);
+    }
+
+    private function processUrl()
+    {
+        $_GET['url'] =isset($_GET['url']) ? filter_var(trim($_GET['url'],'/'),FILTER_SANITIZE_URL) : "";
+        $url = explode('/',$_GET['url']);
+
+        //TODO ISSET LANGUAGE AND ADMIN
+        $sUrl = $_GET['url'];
+        //TODO ISSET LANGUAGE AND ADMIN
+        $this->uri = !empty($sUrl) ? $sUrl : $this->uri;
+        $this->url = URL . trim($_SERVER['REQUEST_URI'],'/');
+
     }
 
 }
